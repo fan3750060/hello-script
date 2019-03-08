e64c92341ea0f09a5979661512e6961787bd87db
fan3750060 <fan3750060@163.com>
2019-03-08 13:57:06 +0800

    1

--- core/db/Redis.php
+++ core/db/Redis.php
@@ -103,7 +103,7 @@ class Redis
             return $default;
         }
         $jsonData = json_decode($value, true);
-        // 检测是否为JSON数据 true 返回JSON解析数组, false返回源数据 byron sampson<xiaobo.sun@qq.com>
+        // 检测是否为JSON数据 true 返回JSON解析数组, false返回源数据
         return (null === $jsonData) ? $value : $jsonData;
     }
 
@@ -128,7 +128,7 @@ class Redis
             $first = true;
         }
         $key = $this->getCacheKey($name);
-        //对数组/对象数据进行缓存处理，保证数据完整性  byron sampson<xiaobo.sun@qq.com>
+        //对数组/对象数据进行缓存处理，保证数据完整性 
         $value = (is_object($value) || is_array($value)) ? json_encode($value) : $value;
         if (is_int($expire) && $expire) {
             $result = $this->handler->setex($key, $expire, $value);
